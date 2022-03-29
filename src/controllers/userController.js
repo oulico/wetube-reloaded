2de06c431c33c4c98e0bb6f524b66125b8dd5d36
manemis <manemis@gmail.com>
2022-03-29 18:17:32 +0900

    updated login/join in feature

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -56,7 +56,8 @@ export const postLogin = async (req, res) => {
       errorMessage: "Wrong password",
     });
   }
-  console.log("Log user in! coming soon!");
+  req.session.loggedIn = true;
+  req.session.user = user;
   return res.redirect("/");
 };
 export const logout = (req, res) => res.send("Log out");
--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-
+import { localsMiddleware } from "./localsMiddlewares";
 const PORT = 4000;
 
 const app = express();
@@ -13,6 +14,22 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
