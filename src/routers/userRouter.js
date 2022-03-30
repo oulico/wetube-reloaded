295d3c4b221d5e6b93b90dd97c68f5dfcce29c42
manemis <manemis@gmail.com>
2022-03-30 19:13:35 +0900

    add github authentication and logout feature

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController";
+import {
+  edit,
+  logout,
+  see,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../controllers/userController";
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get(":id", see);
 userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 
 export default userRouter;
