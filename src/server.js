94afc72672b8e311e47da30b2a925df67df896ff
manemis <manemis@gmail.com>
2022-04-08 12:26:05 +0900

    added thumbnail and ffmpeg video converter

--- src/server.js
+++ src/server.js
@@ -26,6 +26,13 @@ app.use(
     store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
+
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
+
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"));
