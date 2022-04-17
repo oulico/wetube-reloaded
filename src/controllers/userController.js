305b19a7dc3104c1befc3cb956926a150d682c1b
manemis <manemis@gmail.com>
2022-04-18 00:27:39 +0900

    fixing login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -57,8 +57,7 @@ export const postLogin = async (req, res) => {
   }
   req.session.loggedIn = true;
   // req.session.user = user;
-  // return res.redirect("/");
-  console.log("is it ok?");
+  return res.redirect("/");
 };
 
 export const startGithubLogin = (req, res) => {
