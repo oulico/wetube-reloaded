f3c3dad8958d5eb479421464b1ecb0bf9e998558
manemis <manemis@gmail.com>
2022-04-17 16:54:38 +0900

    s3 updated

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,20 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: "ewtube",
+  acl: "public-read",
+});
+
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
@@ -25,10 +41,13 @@ export const publicOnlyMiddleware = (req, res, next) => {
 };
 
 export const avatarUpload = multer({
+  limits: { fileSize: 3000000 },
   dest: "uploads/avatars/",
+  storage: multerUploader,
 });
 
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: { fileSize: 10000000 },
+  storage: multerUploader,
 });
